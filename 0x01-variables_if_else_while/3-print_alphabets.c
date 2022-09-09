9a047c695b5a8acaee0e7df18619f88f63add8b7
vector-10 <duzieblaise9@gmail.com>
2022-09-09 15:08:23 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,24 +5,23 @@
  * and then in uppercase, followed by a new line.
  * Return: 0
  */
+
 int main(void)
 
 {
+
 char c = 'a';
 while (c <= 'z')
 {
-
 putchar(c);
 c++;
-
 }
 c = 'A';
 while (c <= 'Z')
 {
-										putchar(c);
+putchar(c);
 c++;
-										}
+}
 putchar('\n');
 return (0);
-
 }
