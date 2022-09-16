5c93439bcaf970fe19973f870a4956892f543b51
vector-10 <duzieblaise9@gmail.com>
2022-09-16 06:54:53 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,95 +1,53 @@
 #include "main.h"
-
 /**
-
- * times_table -prints tables
-
- *
-
- * Return: Always 0.
-
- */
+* times_table -prints tables
+* Return: Always 0.
+*/
 
 void times_table(void)
 
 {
+int a;
+int b;
+int c;
 
-	int a;
+for (a = 0; a <= 9; a++)
+{
 
-	int b;
+for (b = 0; b <= 9; b++)
+{
 
-	int c;
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
 
+}
+if (b != 0)
+{
+_putchar(' ');
+_putchar((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+{
 
-
-	for (a = 0; a <= 9; a++)
-
-	{
-
-		for (b = 0; b <= 9; b++)
-
-		{
-
-			c = a * b;
-
-			if ((c / 10) == 0)
-
-			{
-
-				if (b == 0)
-
-				{
-
-					_putchar ('0');
-
-				}
-
-				if (b != 0)
-
-				{
-
-					_putchar(' ');
-
-					_putchar((c % 10) + '0');
-
-				}
-
-				if (b < 9)
-
-				{
-
-					_putchar(',');
-
-					_putchar(' ');
-
-				}
-
-			}
-
-			else
-
-			{
-
-				_putchar ((c / 10) + '0');
-
-				_putchar ((c % 10) + '0');
-
-				if (b < 9)
-
-				{
-
-					_putchar(',');
-
-					_putchar(' ');
-
-				}
-
-			}
-
-		}
-
-		_putchar ('\n');
-
-	}
-
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar ('\n');
+}
 }
